262bfbdc86ffd4a935a33425deb420ddb9576176
Jade <jade.lillotrynes@gmail.com>
2022-06-20 23:43:41 +0800

    Add basic property lists and cards

---
+++ src/components/PropertyCard.vue
@@ -0,0 +1,62 @@
+<script>
+import UtilityText from "./UtilityText.vue";
+export default {
+  props: {
+    property: { type: Object, default: undefined },
+  },
+  components: { UtilityText },
+};
+</script>
+
+<template>
+  <div class="search-save-card">
+    <div
+      class="card-header"
+      :style="{
+        '--primary': property.agency.brandingColors.primary,
+      }"
+    >
+      <img class="logo-image" :src="property.agency.logo" />
+    </div>
+    <div
+      class="card-image"
+      :style="{
+        'background-image': `url(${property.mainImage})`,
+      }"
+    />
+    <div class="card-footer">
+      <UtilityText heading size="m">{{ property.price }}</UtilityText>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "@/assets/base.css";
+.search-save-card {
+  width: 320px;
+  height: 336px;
+  margin: 0 12px 32px 12px;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+.card-header {
+  height: 32px;
+  padding: 8px;
+  background-color: var(--primary);
+}
+.logo-image {
+  height: 100%;
+}
+.card-image {
+  height: 200px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.card-footer {
+  height: 32px;
+  padding: 12px;
+  background-color: var(--primary);
+}
+</style>
---
+++ src/components/PropertyList.vue
@@ -0,0 +1,29 @@
+<script setup>
+import UtilityText from "./UtilityText.vue";
+import PropertyCard from "../components/PropertyCard.vue";
+</script>
+<script>
+export default {
+  props: {
+    properties: { type: Array, default: undefined },
+    title: { type: String, default: null },
+  },
+};
+</script>
+
+<template>
+  <div class="search-save-list">
+    <UtilityText heading size="m">
+      {{ title }}
+    </UtilityText>
+    <PropertyCard
+      v-for="property in properties"
+      :key="property"
+      :property="property"
+    />
+  </div>
+</template>
+
+<style>
+@import "@/assets/base.css";
+</style>
