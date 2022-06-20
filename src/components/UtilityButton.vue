69eb4fe7006a7d3fcddeaffc0de64578f2d9be31
Jade <jade.lillotrynes@gmail.com>
2022-06-20 22:08:31 +0800

    Add basic button component

---
+++ src/components/UtilityButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+import UtilityText from "../components/UtilityText.vue";
+</script>
+<script>
+export default {
+  props: {
+    label: { type: String, default: null },
+  },
+};
+</script>
+
+<template>
+  <div class="search-save-button">
+    <button type="button">
+      <slot />
+      <UtilityText heading size="s">
+        {{ label }}
+      </UtilityText>
+    </button>
+  </div>
+</template>
+
+<style>
+@import "@/assets/base.css";
+.search-save-button button {
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  border-radius: 8px;
+}
+</style>
