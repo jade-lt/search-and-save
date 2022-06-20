a5885966e6710817d8a2746a1edf0a2f8894ce3f
Brendon LeClerc <brendonleclerc@Brendons-MacBook-Pro.local>
2022-06-20 13:06:26 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Search and Save</h1>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+</style>
